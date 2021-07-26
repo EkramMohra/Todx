c86f34cdba71ebe8e175b62996ecfdab5873a3c9
EkramMohra <mohraekram@gmaill.com>
2021-07-26 10:45:04 +0300

    frontend tasks

--- src/Components/Container/Container.js
+++ src/Components/Container/Container.js
@@ -1,13 +1,26 @@
 import { inject, observer } from 'mobx-react';
 import React from 'react';
 import List from './List'
+import MyModal from './addpopup';
+import { useState } from 'react';
+
 const Container = (props) => {
 
-    console.log(props.list.list[0])
+    const [modalShow, setModalShow] = useState(false)
+
+
     return (
+        <>
+        <MyModal show={modalShow}
+            onHide={() => setModalShow(false)}
+        />
+
         <div>
+
            <List tasks={props.list.list}/>
+           <button onClick={() => setModalShow(true)}>Add</button>
         </div>
+        </>
     );
 };
 
